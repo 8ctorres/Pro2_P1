9635c0de3097606ccb7d04b17a1fa40e3a618b61
Carlos Torres <>
2019-02-24 22:17:05 +0100

    Añadir Comentarios
    Acabé de completar los comentarios de las funciones

--- StaticList.pas
+++ StaticList.pas
@@ -61,17 +61,39 @@ interface
         Salida: La posición del elemento siguiente
         Precondición: La posición indicada es una posición válida*)
 	function previous(p:tPosL; L:tList):tPosL;
+	(*	Objetivo: Devuelve la posición en la lista del elemento anterior al indicado
+		Entrada: Una lista
+		Salida: La posición del elemento anterior
+		Precondición: La posición indicada es una posición válida*)
 	function insertItem(d:tItem;p:tPosL; var L : tList): boolean;
+	(*	Objetivo: Inserta un elemento en la lista antes de la posición indicada, o al final si la posición es NULL
+		Entrada: Un elemento a insertar, una posición y una lista
+		Salida: La lista con el elemento insertado y un boolean TRUE si se ha insertado correctamente y un FALSE en caso contrario
+		Precondición: La posición indicada es una posición válida en la lista, o bien NULL
+		Postcondición: Las posiciones de los elementos de la lista posteriores al insertado pueden cambiar de valor*)
 	procedure deleteAtPosition(p:tPosL; var L : tList);
+	(*	Objetivo: Elimina de la lista el elemento que ocupa la posición indicada
+		Entrada: Una lista y una posición
+		Salida: El elemento en la posición indicada en esa lista
+		Precondición La posición indicada es una posición válida en la lista
+		Postcondición: Tanto la posición del elemento eliminado como aquellas de los elementos de la lista a continuación pueden cambiar de valor*)
 	function getItem(p:tPosL; L: tList):tItem;
+	(*	Objetivo: Devuelve el contenido del elemento de la lista que ocupa la posición indicada
+		Entrada: Una posición y una lista
+		Salida: El elemento en esa posición de la lista
+		Precondición: La posición indicada es una posición válida en la lista*)
 	procedure updateVotes(d: tNumVotes; p: tPosL; var L:tList);
+	(*	Objetivo: Modifica el número de votos del elemento situado en la posición indicada
+		Entrada: El nuevo número de votos, la posición del partido que se quiere modificar, y la lista
+		Salida: La lista con los votos actualizados
+		Precondición: La posición indicada es una posición válida en la lista
+		Postcondición: El orden de los elementos de la lista no se ve modificado*)
 	function findItem(d:tPartyName; L:tList):tPosL; 
-	(*Objetivo: Encuentra el primer partido politico en una lista que coincida
-		con el partido buscado.
-	  Entradas: Un partido político y una lista
-	  Salidas: Una variable de posicion
-	  Precondicion: La lista es no vacia
-	  Poscondicion: Si el partido no se encuentra en la lista la funcion devolvera NULL*)
+	(*	Objetivo: Encuentra el primer partido politico en una lista que coincida con el partido buscado.
+	  	Entradas: Un nombre de partido político y una lista
+	  	Salidas: Una variable de posicion
+	  	Precondicion: La lista es no vacia
+	  	Poscondicion: Si el partido no se encuentra en la lista la funcion devuelve NULL*)
 	
 implementation
 
